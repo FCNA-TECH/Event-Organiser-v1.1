b6ba9901fb34f88de4d2c07230832e8288e536f6
Ferdinand <97032094+FCNA-TECH@users.noreply.github.com>
2024-02-17 18:13:02 +0000

    Create Events.cs

---
+++ Events.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Events
+{
+    public int ID;             //  Could be a potential future add in to help organise and catagorise the events alongwith a string type variable to each event
+    //public string Type;            Possibly come in a future update
+    public string Title;
+    public string Description;
+    public DateTime DueDate;
+}
